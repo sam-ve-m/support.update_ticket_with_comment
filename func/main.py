bcf62fd4ab22c77b14e0a9fafdbd0f3f6e7149b0
Vinicius Reis <vro@lionx.com.br>
2022-02-24 17:31:13 -0300

    service update in progress

--- func/main.py
+++ func/main.py
@@ -3,21 +3,25 @@ from flask import Response, request
 from json import dumps
 from heimdall_client.bifrost import Heimdall
 from src.validator import Ticket
+from src.service import UpdateTicketWithComment
 log = getLogger()
 
 
-def update_new_ticket_comments_in_zendesk(ticket_id):
+def update_new_ticket_comments_in_zendesk():
+    url_path = request.full_path
     x_thebes_answer = request.headers.get('x-thebes-answer')
     heimdall_client = Heimdall(logger=log)
     raw_ticket_params = request.json
-    http_status = 403
     try:
+        http_status = 403
         inserted = False
-        http_status = 200
         if heimdall_client.validate_jwt(jwt=x_thebes_answer):
             jwt_content, heimdall_status = heimdall_client.decode_payload(jwt=x_thebes_answer)
             ticket_params = Ticket(**raw_ticket_params)
-
+            update_comments_service = UpdateTicketWithComment(params=ticket_params, url_path=url_path, x_thebes_answer=jwt_content['decoded_jwt'])
+            update_comments_service.requester_is_the_same_ticket_user()
+            update_comments_service.update_comments_in_zendesk_ticket()
+            http_status = 200
     return Response(
             dumps({"status": inserted}),
             mimetype="application/json",
--- func/src/service.py
+++ func/src/service.py
@@ -1,7 +1,11 @@
+from base64 import b64decode
+from os import SEEK_SET
+from tempfile import TemporaryFile
 from decouple import config
 from zenpy import Zenpy
 from zenpy.lib.api_objects import User, Ticket, Via, Comment, Attachment
-from typing import Optional
+from typing import List, Optional
+from pydantic import BaseModel
 
 
 class UpdateTicketWithComment:
@@ -9,20 +13,20 @@ class UpdateTicketWithComment:
         'email': config('ZENDESK_EMAIL'),
         'password': config('ZENDESK_PASSWORD'),
         'subdomain': config('ZENDESK_SUBDOMAIN')
-    })
+        })
 
-    def __init__(self, params, url_path, x_thebes_answer):
+    def __init__(self, params: BaseModel, url_path: str, x_thebes_answer: dict):
         self.x_thebes_answer = x_thebes_answer
         self.url_path = url_path
         self.params = params.dict()
-        pass
+        #Sindri.dict_to_primitive_types(self.params)
 
     def get_ticket(self) -> Ticket:
         ticket_zenpy = self.zenpy_client.tickets(id=self.params['id'])
         return ticket_zenpy
 
     def get_user(self) -> User:
-        unique_id = self.xthebes_answer['unique_id']
+        unique_id = self.x_thebes_answer['unique_id']
         user_result = self.zenpy_client.users(external_id=unique_id)
         if user_result:
             user_zenpy = user_result.values[0]
@@ -35,12 +39,7 @@ class UpdateTicketWithComment:
         ticket_zenpy = self.get_ticket()
         if ticket_zenpy.requester == user_zenpy:
             return True
-        return False
-
-    def get_comments(self) -> Comment:
-        ticket_zenpy = self.get_ticket()
-        comments_zenpy = self.zenpy_client.tickets.comments(ticket=ticket_zenpy)
-        return comments_zenpy
+        raise Exception('Bad request')
 
     def update_comments_in_zendesk_ticket(self):
         user_zenpy = self.get_user()
@@ -54,3 +53,22 @@ class UpdateTicketWithComment:
         )
         ticket_zenpy.comment = new_comment
         self.zenpy_client.tickets.update(ticket_zenpy)
+
+    def get_attachments(self) -> List[Attachment]:
+        attachment_tokens = list()
+        for attachment in self.params['attachments']:
+            file_bytes = b64decode(attachment['content'])
+            with TemporaryFile() as temp_file:
+                temp_file.write(file_bytes)
+                temp_file.seek(SEEK_SET)
+                upload_instance = self.zenpy_client.attachments.upload(
+                    fp=temp_file,
+                    target_name=attachment['name']
+                )
+                attachment_tokens.append(upload_instance.token)
+        return attachment_tokens
+
+    def get_comments(self) -> Comment:
+        ticket_zenpy = self.get_ticket
+        comments_zenpy = self.zenpy_client.tickets.comments(ticket=ticket_zenpy)
+        return comments_zenpy
