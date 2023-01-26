238ba90778b92e44d58d6e1a317285b17282324c
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-26 10:47:57 -0300

    "build: changed fission strategy to poolmg"

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 fission spec init
-fission env create --spec --name sup-tckt-comment-env --image nexus.sigame.com.br/fission-support-ticket-add-comment:0.1.0-0 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
-fission fn create --spec --name sup-tckt-comment-fn --env sup-tckt-comment-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
+fission env create --spec --name sup-tckt-comment-env --image nexus.sigame.com.br/fission-support-ticket-add-comment:0.1.0-0 --poolsize 2 --graceperiod 3 --version 3 --imagepullsecret "nexus-v3" --spec
+fission fn create --spec --name sup-tckt-comment-fn --env sup-tckt-comment-env --code fission.py --executortype poolmgr --requestsperpod 10000 --spec
 fission route create --spec --name sup-tckt-comment-rt --method PUT --url /support/update-ticket --function sup-tckt-comment-fn
